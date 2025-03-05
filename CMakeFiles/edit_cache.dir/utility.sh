dab63f8e79cae8ec63fe6a0253f360d6f72fd20f
0xBB8 <lampmud@163.com>
2025-03-05 13:46:16 +0800

    remove useless dir

--- CMakeFiles/edit_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /c/Users/Admin/Desktop/f407_cmake_ninja_clangd
-/usr/bin/ccmake.exe -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
--- CMakeFiles/rebuild_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /c/Users/Admin/Desktop/f407_cmake_ninja_clangd
-/usr/bin/cmake.exe --regenerate-during-build -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
