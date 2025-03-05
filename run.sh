c4bf33b1641790e2ead18823fe183e5016ae1c87
0xBB8 <lampmud@163.com>
2025-03-05 12:54:41 +0800

    trying to fix the path error

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ cmake --build . --target all --config Release -- -j 16
 drive_letter=$(pwd | cut -d'/' -f2 | tr '[:lower:]' '[:upper:]')
 
 # 修改 compile_commands.json 文件中的路径
-sed -i "s|/c/|${drive_letter}:|g" compile_commands.json
+sed -i "s|/c/|C:/|g; s|/d/|D:/|g; s|/e/|E:/|g; s|/f/|F:/|g; s|/g/|G:/|g; s|/h/|H:/|g; s|/i/|I:/|g; s|/j/|J:/|g; s|/k/|K:/|g; s|/l/|L:/|g; s|/m/|M:/|g; s|/n/|N:/|g; s|/o/|O:/|g; s|/p/|P:/|g; s|/q/|Q:/|g; s|/r/|R:/|g; s|/s/|S:/|g; s|/t/|T:/|g; s|/u/|U:/|g; s|/v/|V:/|g; s|/w/|W:/|g; s|/x/|X:/|g; s|/y/|Y:/|g; s|/z/|Z:/|g" compile_commands.json
 
 # 获取 .ioc 文件的前缀
 ioc_file=$(basename ../*.ioc .ioc)
